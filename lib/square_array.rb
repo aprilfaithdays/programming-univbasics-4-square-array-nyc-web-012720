5dd41e95c869e2a095ee28f7041615dfe213c418
April Escobar <aprilfaith.e@gmail.com>
2020-01-08 21:29:57 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,12 +2,13 @@ def square_array(array)
   
   squared_array = []
   num = 0
+ 
   
   while num < array.length do
-  squared_array.push(array[num]*num)
+  squared_array.push(array[num]*array[num])
   
   num += 1
   
   return squared_array
-
+end
 end
